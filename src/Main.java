6b0f7b1413416458c0ddd2c8d8d3831b8fdba2e8
Ольга <koval.oi.1106@gmail.com>
2022-09-01 19:44:05 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // задание 1
+
+      String firstName = " Иван" ;
+      String middleName = " Иванович" ;
+      String lastName = " Иванов " ;
+      String fullName = lastName + firstName + middleName  ;
+      System.out.println ( " Иванов Иван Иванович");
+
     }
 }
